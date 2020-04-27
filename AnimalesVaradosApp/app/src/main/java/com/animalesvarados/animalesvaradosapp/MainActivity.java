643126dd1ca893e66467e8a9355611e262655951
AlejandroGH97 <alejandrogoicochea@gmail.com>
2020-04-27 09:48:18 -0500

    fix(Main activity methods): Changed Main Activity methods to fix an error.

--- AnimalesVaradosApp/app/src/main/java/com/animalesvarados/animalesvaradosapp/MainActivity.java
+++ AnimalesVaradosApp/app/src/main/java/com/animalesvarados/animalesvaradosapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("Registered")
@@ -15,19 +17,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Reporte animales varados");
+
+        final Button loginbtn = findViewById(R.id.go_login);
+        final Button regbtn = findViewById(R.id.go_register);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Go_login();
+            }
+        });
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Go_register();
+            }
+        });
     }
 
     public Activity getActivity(){
         return this;
     }
 
-    public void Go_login(View view){
+
+    public void Go_login(){
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         startActivity(intent);
     }
 
 
-    public void Go_register(View view){
+    public void Go_register(){
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         startActivity(intent);
     }
